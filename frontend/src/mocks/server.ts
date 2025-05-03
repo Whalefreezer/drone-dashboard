58a38605b04136eb5c35d82b8b548da416ff75eb
Robert Oram <roboenator.corp@gmail.com>
2025-05-03 16:22:38 +1200

    Fix tests

--- frontend/src/mocks/server.ts
+++ frontend/src/mocks/server.ts
@@ -1,10 +1,10 @@
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers.ts';
+import { BASE_URL, handlers } from './handlers.ts';
 import axios from 'axios';
 
 // This configures a request mocking server with the given request handlers.
 export const server = setupServer(...handlers); 
 
 
-axios.defaults.baseURL = 'http://localhost';
+axios.defaults.baseURL = BASE_URL;
 
