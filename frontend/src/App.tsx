1483702d73228d375e057559d47f337ed5f23e8a
Robert Oram <roboenator.corp@gmail.com>
2025-05-04 18:04:07 +1200

    Cleanup app.tsx some more

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
-import { useAtomValue, useSetAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
 import {
-    racesAtom,
     roundsDataAtom,
     usePeriodicUpdate,
 } from './state/index.ts';
@@ -15,7 +14,6 @@ import { Leaderboard } from './leaderboard/Leaderboard.tsx';
 import { EliminatedPilotsView } from './bracket/index.ts';
 
 function App() {
-    const races = useAtomValue(racesAtom);
     const updateRoundsData = useSetAtom(roundsDataAtom);
 
     usePeriodicUpdate(updateRoundsData, 10_000);
@@ -28,20 +26,11 @@ function App() {
             <SnapshotControl />
 
             <div
-                style={{
-                    textAlign: 'center',
-                    borderBottom: '1px solid #333',
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    backgroundColor: '#1a1a1a',
-                    zIndex: 100,
-                }}
+                className='app-header'
             >
                 <TimeDisplay />
             </div>
-            <div className='app-container' style={{ marginTop: '40px' }}>
+            <div className='app-main-content' >
                 <RacesContainer />
                 <Leaderboard />
                 <EliminatedPilotsView />
