7c67ac89321002ce7d3c26068a708edd95e54bbb
Whalefreezer <roboenator.corp@gmail.com>
2025-09-23 19:41:15 +1200

    refactor(routes): conditionally render navigation based on mobile breakpoint

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,23 +1,30 @@
 import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import useBreakpoint from '../responsive/useBreakpoint.ts';
 
 type RouterContext = Record<PropertyKey, never>;
 
 const enableRouterDevtools = import.meta.env.DEV && (import.meta.env.VITE_ROUTER_DEVTOOLS === 'true');
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-	component: () => (
-		<>
-			<nav style={{ padding: 8, display: 'flex', gap: 12 }}>
-				{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
-				<Link to='/'>Dashboard</Link>
-				{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
-				<Link to='/admin'>Admin</Link>
-			</nav>
-			<main>
-				<Outlet />
-			</main>
-			{enableRouterDevtools && <TanStackRouterDevtools position='bottom-right' />}
-		</>
-	),
+	component: () => {
+		const { isMobile } = useBreakpoint();
+
+		return (
+			<>
+				{!isMobile && (
+					<nav style={{ padding: 8, display: 'flex', gap: 12 }}>
+						{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
+						<Link to='/'>Dashboard</Link>
+						{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
+						<Link to='/admin'>Admin</Link>
+					</nav>
+				)}
+				<main>
+					<Outlet />
+				</main>
+				{enableRouterDevtools && <TanStackRouterDevtools position='bottom-right' />}
+			</>
+		);
+	},
 });
