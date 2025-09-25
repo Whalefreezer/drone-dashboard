230291c7a9f5896018b75d10aa0ab8c70740a24e
Whalefreezer <roboenator.corp@gmail.com>
2025-09-25 17:16:40 +1200

    refactor(router): remove mobile navigation links from root route

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -23,14 +23,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 
 		return (
 			<>
-				{!isMobile && (
-					<nav style={{ padding: 8, display: 'flex', gap: 12 }}>
-						{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
-						<Link to='/'>Dashboard</Link>
-						{/* @ts-ignore - TanStack Router type issue, see https://github.com/denoland/deno/issues/30444 */}
-						<Link to='/admin'>Admin</Link>
-					</nav>
-				)}
 				<main>
 					<Outlet />
 				</main>
